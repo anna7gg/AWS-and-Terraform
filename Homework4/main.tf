e23e64cb7a5cf3aa5b1fb424613c65cb165103a6
anna7gg <38318504+anna7gg@users.noreply.github.com>
2022-03-17 16:17:26 +0200

    Update main.tf

--- Homework4/main.tf
+++ Homework4/main.tf
@@ -1,10 +1,3 @@
-# S3 bucket config
-module "bucket" {
-  bucket_name = var.bucket_name
-  source = ".//Modules//read_write_s3"
-  canned_acl = var.canned_acl
-}
-
 module "vpc" {
   source = ".//Modules//vpc"
   private_subnet = var.private_subnet
