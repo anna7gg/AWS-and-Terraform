d201085772a69eccaca70cef79efeb1ace5a6b20
anna7gg <annag@pixellot.tv>
2022-03-17 11:56:11 +0200

    state

--- Homework4/instance.tf
+++ Homework4/instance.tf
@@ -17,22 +17,7 @@ resource "aws_instance" "web" {
   tags = {
     Name = "webServer-${count.index}-${data.aws_availability_zones.available.names[count.index]}"
     }
-}
-###### db
-resource "aws_instance" "DB_instances" {
-  count                       = "${var.DB_instances_count}"
-  ami                         = "${data.aws_ami.latest-ubuntu.id}"
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = module.vpc.private_subnets_id[count.index]
-  associate_public_ip_address = false
-  vpc_security_group_ids      = [aws_security_group.db_security_group.id]
-
-  tags = {
-    "Name" = "DB-${data.aws_availability_zones.available.names[count.index]}"
-  }
-}
-##### volume
+}##### volume
 resource "aws_volume_attachment" "ebs_att" {
   count = 2
   device_name = "/dev/sdh"
@@ -86,39 +71,3 @@ resource "aws_security_group_rule" "webserver_outbound_anywhere" {
     type              = "egress"
     cidr_blocks       = ["0.0.0.0/0"]
 }
-
-resource "aws_security_group" "db_security_group" {
-    name = "db_security_group"
-    vpc_id = module.vpc.vpc_id
-        tags = {
-        Name = "db_security_group ${module.vpc.vpc_id}"
-    }
-}
-
-
-resource "aws_security_group_rule" "DB_ssh_acess" {
-    description       = "allow ssh access from anywhere"
-    from_port         = 22
-    protocol          = "tcp"
-    security_group_id = aws_security_group.db_security_group.id
-    to_port           = 22
-    type              = "ingress"
-    cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "DB_outbound_anywhere" {
-    description       = "allow outbound traffic to anywhere"
-    from_port         = 0
-    protocol          = "-1"
-    security_group_id = aws_security_group.db_security_group.id
-    to_port           = 0
-    type              = "egress"
-    cidr_blocks       = ["0.0.0.0/0"]
-
-}
-
-
-
-
-
-
--- Homework4/main.tf
+++ Homework4/main.tf
@@ -4,10 +4,3 @@ module "bucket" {
   source = ".//Modules//read_write_s3"
   canned_acl = var.canned_acl
 }
-
-module "vpc" {
-  source = ".//Modules//vpc"
-  private_subnet = var.private_subnet
-  public_subnet = var.public_subnet
-  vpc_cidr_block = var.vpc_cidr_block
-}
